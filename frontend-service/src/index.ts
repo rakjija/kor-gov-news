fd871a14ab5f021125a4cbeb0cf820f90d568379
rakjija <minjai100111@gmail.com>
2025-07-09 20:50:26 +0900

    feat: add login POST handler

--- frontend-service/src/index.ts
+++ frontend-service/src/index.ts
@@ -9,8 +9,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/login', (req, res) => {
+app.get('/login', (_, res) => {
   res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  console.log(`Login attempt: ${username}, ${password}`);
+  res.send('Processing login');
 });
 
 app.listen(PORT, () => {
